f377623caeb3c40e39bed083b8eeba97c24d1966
gideonadjei <gideonadjei94@gmail.com>
2025-01-08 01:17:18 +0000

    Initial Commit

---
+++ Services/order/src/main/java/com/ecommerce/order/Models/Order.java
@@ -0,0 +1,11 @@
+package com.ecommerce.order.Models;
+
+import java.math.BigDecimal;
+
+public class Order {
+    private Integer id;
+    private String reference;
+    private BigDecimal totalAmount;
+    private String customerId;
+
+}
